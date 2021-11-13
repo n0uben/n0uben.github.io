9e584618ff82bb96a56b6da61b56a7c784e4e0d6
n0uben <benjamin.noureux@gmail.com>
2021-11-13 12:09:11 +0100

    add comments to js file

--- mon_super_fichier_js.js
+++ mon_super_fichier_js.js
@@ -4,19 +4,18 @@ langage que j’ai eu l’occasion de pratiquer. */
 
 // on récupère le bouton mobile & les liens du menu
 let toggleMenu = document.querySelectorAll('#menu-mobile-button, .menu-item a');
+//on isole le bouton menu mobile pour agir dessus indépendamment
 let boutonMenuMobile = toggleMenu[0];
-//on récupère le menu
+//on récupère le menu en lui meme
 let menuMobile = document.getElementById('header-menu-nav');
 
 // on ajouter un ecouteur d’évenement sur le bouton mobile & les liens du menu
-longueur = toggleMenu.length
-
-for (let i = 0; i < longueur; i++) {
+for (let i = 0; i < toggleMenu.length; i++) {
     
     toggleMenu[i].addEventListener('click', function() {
     
         //quand on clique sur le bouton mobile, ajoute une classe au menu mobile pour qu’il s’affiche
-        //on récupère le nombre de class css du menu
+        //on vérifie si la classe visible est présente
         is_visible = (menuMobile.classList.contains("visible"));
         
         //s’il y en a une, le menu est visible
