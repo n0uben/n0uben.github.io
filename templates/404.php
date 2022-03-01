64ae19d9368a0d11a29f15900aa961426d007152
n0uben <benjamin.noureux@gmail.com>
2022-03-01 21:42:48 +0100

    create small template system

---
+++ templates/404.php
@@ -0,0 +1,14 @@
+<div class="section">
+    <div class="row">
+        <div class="col">
+        <h1>ERREUR 404</h1>
+
+            <p>la page que vous avez demandé n’existe pas !</p>
+
+            <p>
+                <a href="/">Retourner à l'accueil</a>
+            </p>
+            
+        </div>
+    </div>
+</div class="section">
