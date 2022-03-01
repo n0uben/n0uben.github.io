58fe99f961ce6bd6b0f55248b272eea822fd046d
n0uben <benjamin.noureux@gmail.com>
2022-03-01 20:36:31 +0100

    add  test

--- index.php
+++ index.php
@@ -6,22 +6,25 @@
         
             $page = $_SERVER['REQUEST_URI'];
 
-            echo $page;
-
-            switch ($page) {
-
-                case '/n0uben.github.io/mentions-legales.php':
-                    include('./mentions-legales.php');
-                    break;
-                
-                case '/n0uben.github.io/index.php':
-                    include('./home.php');
-                    break;
-                
-                default:
-                    include('./home.php');
-                    break;
+            if ($page == '/n0uben.github.io/mentions-legales.php') {
+                include('mentions-legales.php');
+            } else {
+                include('home.php');
             }
+            // switch ($page) {
+
+            //     case '/n0uben.github.io/mentions-legales.php':
+            //         include('./mentions-legales.php');
+            //         break;
+                
+            //     case '/n0uben.github.io/index.php':
+            //         include('./home.php');
+            //         break;
+                
+            //     default:
+            //         include('./home.php');
+            //         break;
+            // }
              
 
         ?>
