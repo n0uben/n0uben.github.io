b654b498fee84cfebba302d1a1d2a56bedcf10f8
n0uben <benjamin.noureux@gmail.com>
2022-03-02 18:31:53 +0100

    add ./ at beginning of includes to fix links (hopefully)

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
   
-    <?php include("templates/header.php") ?>
+    <?php include("./templates/header.php") ?>
 
     <main>
         <?php 
@@ -7,29 +7,29 @@
             $page = $_SERVER["REQUEST_URI"];
 
             if (!$page) {
-                include(".templates/404.php");
+                include("../templates/404.php");
             }
 
             switch ($page) {
 
                 // Accueil
                 case '/':
-                    include('templates/home.php');
+                    include('./templates/home.php');
                     break;
                 case '/index.php':
-                    include('templates/home.php');
+                    include('./templates/home.php');
                     break;
 
                 // Mentions legales
                 case '/mentions-legales':
-                    include('templates/mentions-legales.php');
+                    include('./templates/mentions-legales.php');
                     break;
                 
                 default:
-                    include("templates/404.php");
+                    include("./templates/404.php");
                     break;
             }
         ?>
     </main>
     
-    <?php include("templates/footer.php"); ?>
+    <?php include("./templates/footer.php"); ?>
